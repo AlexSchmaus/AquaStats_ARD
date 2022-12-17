73ec7831bc5b94986aa501e62a36687f7a70eea8
Alex Schmaus <94810049+AlexSchmaus@users.noreply.github.com>
2022-12-17 13:15:40 -0500

    Initial commit

---
+++ AquaStats.ino/AquaStats.ino.ino
@@ -0,0 +1,31 @@
+// 
+// Alex Schmaus
+// AquaStats controller board code
+// 
+// 12/17/22 init
+//
+
+// ToDo:
+// [X] Start
+// [ ] Everything else!
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello World!");
+
+  // Set up sensors
+
+  // Set up WiFi
+
+}
+
+void loop() {
+  
+  Serial.println("Ok");
+  
+  // get readings from the sensor(s)
+  // Make API call, send to AquaStats DB
+  // Wait on the interval - really, maybe this should be tied to a RTC
+
+}
